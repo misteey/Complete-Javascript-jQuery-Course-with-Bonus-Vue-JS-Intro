9d94a6da208452510fa31b15fe1c99d8e1ab0117
Ana <lainrpgs@gmail.com>
2022-04-19 19:19:16 +0200

    Create scripts.js

---
+++ First steps with Javascript/scripts.js
@@ -0,0 +1,19 @@
+// alert("testing the alert");
+
+console.log("statement 1");
+
+console.log("statement 2"); 
+
+
+// this is a comment
+
+/*
+
+this
+is
+a 
+comment
+
+*/
+
+// alert("testing the alert");
