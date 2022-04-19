7c22573ade54a3fbeacd548b2764e6c4ecaab36c
Ana <lainrpgs@gmail.com>
2022-04-19 19:19:51 +0200

    Create working with the dom.js

---
+++ First steps with Javascript/working with the dom.js
@@ -0,0 +1,11 @@
+document.getElementById("blue_box").innerHTML;
+
+document.getElementById("blue_box").innerHTML = "test";
+
+document.getElementById("blue_box").innerHTML = "<h1>test</h1>";
+
+console.log( document.getElementById("blue_box").innerHTML );
+
+document.getElementById("blue_box").innerHTML = "<h1>Testing</h1>";
+
+console.log( document.getElementById("blue_box").innerHTML );
