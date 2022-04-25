048ff9a29851a12d2dfaf726b25a851a95d690af
Ana <lainrpgs@gmail.com>
2022-04-25 10:30:51 +0200

    Update variables.js

--- Basics of Javascript/variables.js
+++ Basics of Javascript/variables.js
@@ -16,3 +16,21 @@ console.log(firstName[0]); // The result will be "J"
 
 var url = "https://www.udemy.com";
 console.log( url.replace( "https://" , "") ); // it will show "www.udemy.com"
+
+var birthYear = 1997;
+var price = 16.99;  // decimal places must be separated by a dot
+var temperature = -5;
+
+var bigNumber = 2e45;
+/* large numbers can be represented by the exponential notation
+// this example means 2*10^45 */
+
+var num1 = 20;
+var num2 = 3;
+
+var sum = num1 + num2;
+var subtraction = num1 - num2;
+var division = num1 / num2;
+var multiplication = num1 * num2;
+
+var average = (num1 + num29 / 2;
