cb1e9cb308595ac6dadf79a162e913334a82dcee
Ana <lainrpgs@gmail.com>
2022-04-29 20:00:39 +0200

    Create null_and_undefined.js

---
+++ Basics of Javascript/null_and_undefined.js
@@ -0,0 +1,8 @@
+var noValue;
+console.log(noValue); // undefined
+console.log(typeOf noValue); // undefined
+
+var firstName = "Peter";
+console.log(name[0]); // console will show "P"
+console.log(name[4]); // console will show "r"
+console.log(name[10]); // console will show undefined
