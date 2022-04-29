cd3b6d5980e4d015f6ce1edcab9879b84dc7ed25
Ana <lainrpgs@gmail.com>
2022-04-29 20:08:15 +0200

    Update null_and_undefined.js

--- Basics of Javascript/null_and_undefined.js
+++ Basics of Javascript/null_and_undefined.js
@@ -1,8 +1,19 @@
 var noValue;
 console.log(noValue); // undefined
-console.log(typeOf noValue); // undefined
+console.log(typeof noValue); // undefined
 
 var firstName = "Peter";
 console.log(name[0]); // console will show "P"
 console.log(name[4]); // console will show "r"
 console.log(name[10]); // console will show undefined
+
+
+var temperature = 35;
+console.log(temperature);
+
+temperature = null;
+console.log(temperature); // the console will show null
+console.log(typeof temperature); // the console will show object, could be misleading
+
+temperature = undefined;
+console.log(temperature); // the console will show undefined
