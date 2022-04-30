fd3d7eeebaf98b7f3ca225fd1b471dcaba385f3b
Ana <lainrpgs@gmail.com>
2022-04-30 21:52:52 +0200

    Create functions.js

---
+++ Basics of Javascript/functions.js
@@ -0,0 +1,13 @@
+// creating the function
+
+function sum_numbers() {
+  var num1 = 5;
+  var num2 = 2;
+  var sum = num1 + num2;
+  console.log(sum);
+}
+
+// the function is only executed after being invoked:
+
+sum_numbers(); // the console will show 7
+ 
